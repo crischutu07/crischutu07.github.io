d95ca86e7510cdb150b3c347ad0a07dac01abef0
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-05-05 05:17:59 +0700

    It's too damn long for making these

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-const year = new Date().getFullYear();
-// i still fucked up
-document.getElementById("small-name-ages").innerText = `${year-2007}`;
+// Main script for index.js
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  alert("If you're visiting this website with a mobile device, it kinda terrible since i build this site all for desktop user\n"
+    + "It is recommended to switching to desktop mode.");
+}
+const age = new Date().getFullYear() - 2007;
+const smallInfo = document.getElementById("small-info-ages")
+smallInfo.innerText = `Ages: ${age}`
