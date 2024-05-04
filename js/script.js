a0e164728fcee49b0308f85a11665a32575cc461
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-05-05 05:26:16 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,2 @@
-// Main script for index.js
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  alert("If you're visiting this website with a mobile device, it kinda terrible since i build this site all for desktop user\n"
-    + "It is recommended to switching to desktop mode.");
-}
-const age = new Date().getFullYear() - 2007;
-const smallInfo = document.getElementById("small-info-ages")
-smallInfo.innerText = `Ages: ${age}`
+// Insert advertising
+document.getElementsByName("html").innerHTML += '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3074036642049872" crossorigin="anonymous"></script>'
