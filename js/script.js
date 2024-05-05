08f4f6b1540c5f700c10c43e96124121dc6275a4
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-05-05 08:19:39 +0700

    no more blog

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 // Insert advertising
-document.getElementsByName("html").innerHTML += '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3074036642049872" crossorigin="anonymous"></script>'
+document.head.innerHTML += '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3074036642049872" crossorigin="anonymous"></script>\n' +
+  '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">\n' +
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script><script>hljs.highlightAll();</script>'
