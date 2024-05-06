72a29dc27bc6dc5bb2c82a2b736df69e42f77190
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2024-05-06 16:02:34 +0000

    Umm

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@ const a = document.getElementById("device")
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   a.innerText = "Anyways hope there's not any problem after i fixed everything for mobile.. :) (plot twist: desktop users can't see this)"
 }
-const age = new Date().getFullYear() - 2007;
-const smallInfo = document.getElementById("small-info-ages")
-smallInfo.innerText = `Ages: ${age}`
-// umm
+const date = new Date()
+const birthday = new Date(`May 21 ${date.getFullYear()+1}`);
+const ages = document.getElementById("ages")
+// while(true){
+//   setInterval(function() {
+//     ages.innerText = `Ages: ${birthday} (${birthday.getFullYear()-date.getFullYear()})`
+//   }, 1000)
+// }
+ages.innerText = `Ages: ${birthday.getFullYear()-2007} (${birthday.getFullYear()-date.getFullYear()})`
