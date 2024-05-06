5619bc490671f5044dd68236c0ff8fce333d2164
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-05-06 07:54:33 +0700

    umm

--- js/script.js
+++ js/script.js
@@ -1,4 +1,1 @@
-// Insert advertising
-document.head.innerHTML += '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3074036642049872" crossorigin="anonymous"></script>\n' +
-  '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">\n' +
-  '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script><script>hljs.highlightAll();</script>'
+// I've added up nothing
